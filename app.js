fe958cdf6425abf58204adb372c58a770e3dd97b
DenisZ <zayczev2012@mail.ru>
2021-11-05 20:41:25 +0500

    FinalVueTODO

--- app.js
+++ app.js
@@ -8,15 +8,18 @@ const App = {
     }
   },
   methods: {
-    inputChangeHandler(event) {
-      this.inputValue = event.target.value
-    },
+
     addNewNote() {
       if (this.inputValue !== '') {
         this.notes.push(this.inputValue)
         this.inputValue = ""
       }
     },
+
+    toUpperCase(item) {
+      return item.toUpperCase()
+    },
+
     removeNote(idx) {
       console.log('removeNote', idx)
       this.notes.splice(idx, 1)
@@ -26,6 +29,18 @@ const App = {
     //     this.addNewNote()
     //   }
     // }
+  },
+  computed: {
+    doubleCountComputed() {
+      return this.notes.length * 2
+    },
+  },
+  watch: {
+    inputValue(value) {
+      if  (value.length > 10){
+        this.inputValue = ''
+      }
+    }
   }
 }
 
