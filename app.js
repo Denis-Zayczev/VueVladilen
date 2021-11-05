de353e51ddc424fbb737b2436d8533c5a8fb7ca1
DenisZ <zayczev2012@mail.ru>
2021-11-05 17:46:58 +0500

    TODO10320

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 const App = {
   data() {
     return {
-      counter: 0,
-      title: "Счётчик"
+      placeholderString: "Введите название заметки",
+      title: "Список заметок",
+      inputValue: '',
+      notes: ['Заметка 1', 'Заметка 2']
     }
+  },
+  methods: {
+    inputChangeHandler(event) {
+      this.inputValue = event.target.value
+    },
+    addNewNote() {
+      if (this.inputValue !== '') {
+        this.notes.push(this.inputValue)
+        this.inputValue = ""
+      }
+    },
+    removeNote(idx) {
+      console.log('removeNote', idx)
+      this.notes.splice(idx, 1)
+    }
+    // inputKeyPress(event){
+    //   if (event.key === 'Enter') {
+    //     this.addNewNote()
+    //   }
+    // }
   }
 }
 
